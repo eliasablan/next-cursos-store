40c237efa4375fa267bc9110d470dac61d824c37
Elias Ablan <eliasablan93@gmail.com>
2025-01-14 00:07:41 -0400

    remove unused files

--- src/hooks/use-uploadthing.tsx
+++
@@ -1,5 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import { type UploadThingFileRouter } from "@/app/api/uploadthing/core";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<UploadThingFileRouter>();
