b194e3a47fe0d6f48b90320cfc5a23eefb9229ed
Elias Ablan <eliasablan93@gmail.com>
2025-09-25 20:11:48 -0400

    refactor courses cards links

--- src/app/(app)/cursos/_components/CoursesCatalog.tsx
+++ src/app/(app)/cursos/_components/CoursesCatalog.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { api } from "@/trpc/server";
 import { es } from "date-fns/locale";
 import { format } from "date-fns";
-import { School } from "lucide-react";
+// import { School } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -29,52 +29,47 @@ export default async function CoursesGrid({
       {courses.map(async (course) => {
         const teacher = await api.user.getUserById({ id: course.ownerId });
         return (
-          <Card key={course.id} className="flex flex-col justify-between">
-            <CardHeader>
-              <CardTitle>{course.name}</CardTitle>
-              <CardDescription>{course.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Link href={`/cursos/${course.slug}`}>
-                <div className="relative my-4 flex flex-col items-center justify-center gap-4">
-                  <div className="z-10 flex w-full flex-col items-start justify-center gap-2">
-                    <div>
-                      <Label className="text-muted-foreground">Profesor</Label>
-                      <p className="font-semibold">{teacher?.name}</p>
-                    </div>
-                    <div>
-                      <Label className="text-muted-foreground">
-                        Fecha de inicio
-                      </Label>
-                      <p className="font-semibold">
-                        {format(new Date(course.startDate), "PPP", {
-                          locale: es,
-                        })}
-                      </p>
-                    </div>
-                    <div>
-                      <Label className="text-muted-foreground">
-                        Fecha de finalización
-                      </Label>
-                      <p className="font-semibold">
-                        {format(new Date(course.endDate), "PPP", {
-                          locale: es,
-                        })}
-                      </p>
-                    </div>
+          <Link key={course.id} href={`/cursos/${course.slug}`}>
+            <Card className="flex flex-col justify-between">
+              <CardHeader>
+                <CardTitle>{course.name}</CardTitle>
+                <CardDescription>{course.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="z-10 flex w-full flex-col items-start justify-center gap-2">
+                  <div>
+                    <Label>Profesor</Label>
+                    <p className="font-semibold text-accent">{teacher?.name}</p>
                   </div>
-                  <School className="absolute right-0 top-0 h-28 w-28 text-primary" />
+                  <div>
+                    <Label>Fecha de inicio</Label>
+                    <p className="font-semibold text-accent">
+                      {format(new Date(course.startDate), "PPP", {
+                        locale: es,
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <Label>Fecha de finalización</Label>
+                    <p className="font-semibold text-accent">
+                      {format(new Date(course.endDate), "PPP", {
+                        locale: es,
+                      })}
+                    </p>
+                  </div>
                 </div>
-              </Link>
-            </CardContent>
-            {openSubscribtion && session && teacher?.id !== session.user.id && (
-              <CardFooter className="h-fit">
-                <div className="flex w-full justify-end">
-                  <SubscribeCourseButton courseId={course.id} />
-                </div>
-              </CardFooter>
-            )}
-          </Card>
+              </CardContent>
+              {openSubscribtion &&
+                session &&
+                teacher?.id !== session.user.id && (
+                  <CardFooter className="h-fit">
+                    <div className="flex w-full justify-end">
+                      <SubscribeCourseButton courseId={course.id} />
+                    </div>
+                  </CardFooter>
+                )}
+            </Card>
+          </Link>
         );
       })}
     </div>
